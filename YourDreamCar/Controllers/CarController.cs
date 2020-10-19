32bff47111f24f1a1b40d9c57660b548c6927631
Luca Bulles <lucabulles@gmail.com>
2020-10-19 13:12:39 +0200

    Added Admin view

--- YourDreamCar/Controllers/CarController.cs
+++ YourDreamCar/Controllers/CarController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using YourDreamCar.Models;
+using YourDreamCarInterfaces.Logic;
 
 namespace YourDreamCar.Controllers
 {
     public class CarController : Controller
     {
-        public IActionResult Index()
+        private readonly ICarLogic carLogic;
+        public CarController(ICarLogic _carLogic)
         {
-            return View();
+            this.carLogic = _carLogic;
+        }
+        public IActionResult Admin()
+        {
+            var allCars = carLogic.GetAllCars();
+            var cars = new List<CarViewModel>();
+            foreach (var car in allCars)
+            {
+                cars.Add(new CarViewModel
+                {
+                    Id = car.Id,
+                    Name = car.Name,
+                    Model = car.Model,
+                    HorsePower = car.HorsePower,
+                    Price = car.Price,
+                    Year = car.Year,
+                    Description = car.Description,
+                    ImageSrc = car.ImageSrc
+                }
+                );
+            }
+            return View(cars);
         }
     }
 }
