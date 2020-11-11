0abc0a7906ea80e677e501f70ea1b27bdc8a160d
Luca Bulles <lucabulles@gmail.com>
2020-11-11 14:16:08 +0100

    Added HttpGet and HttpPost methods in AccountController

--- YourDreamCar/Controllers/AccountController.cs
+++ YourDreamCar/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using YourDreamCar.Models;
 using YourDreamCarInterfaces.Logic;
 
 namespace YourDreamCar.Controllers
@@ -16,6 +17,18 @@ namespace YourDreamCar.Controllers
         }
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CreateUser()
+        {
+            var accountViewModel = new AccountViewModel();
+            return View(accountViewModel);
+        }
+        [HttpPost]
+        public ActionResult CreateUser(AccountViewModel account)
+        {
+            accountRegisterLogic.CreateUser(account);
             return View();
         }
     }
--- YourDreamCar/Models/AccountViewModel.cs
+++ YourDreamCar/Models/AccountViewModel.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YourDreamCarInterfaces.DAL;
 
 namespace YourDreamCar.Models
 {
-    public class AccountViewModel
+    public class AccountViewModel : IAccount
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
