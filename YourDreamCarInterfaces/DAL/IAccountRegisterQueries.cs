2ba914c419185957516a88d15b31ad61d788ea69
Luca Bulles <lucabulles@gmail.com>
2020-11-11 14:02:21 +0100

    Added Account and AccountRegisterLogic in Logiclayer

---
+++ YourDreamCarInterfaces/DAL/IAccountRegisterQueries.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YourDreamCarInterfaces.DAL
+{
+    public interface IAccountRegisterQueries
+    {
+        void CreateUser(IAccount account);
+    }
+}
---
+++ YourDreamCarLogic/Account/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YourDreamCarInterfaces.DAL;
+
+namespace YourDreamCarLogic.Account
+{
+    public class Account : IAccount 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
