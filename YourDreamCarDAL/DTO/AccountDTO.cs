66b1f946c275c992eab27c5f3228d0a88f62c048
Luca Bulles <lucabulles@gmail.com>
2020-11-11 13:36:17 +0100

    Added AccountDTO and IAccount interface

--- YourDreamCarDAL/DTO/AccountDTO.cs
+++ YourDreamCarDAL/DTO/AccountDTO.cs
@@ -2,7 +2,19 @@
 
 namespace YourDreamCarDAL
 {
-    public class Class1
+    public class AccountDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
     }
 }
