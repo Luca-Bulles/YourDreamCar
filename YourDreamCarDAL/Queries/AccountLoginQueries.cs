633219c901c6c450c9152a2d728e1878efde3e77
Luca Bulles <lucabulles@gmail.com>
2020-11-13 15:48:57 +0100

    Added AccountLoginQueries in DAL

---
+++ YourDreamCarDAL/Queries/AccountLoginQueries.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YourDreamCarDAL.Queries
+{
+    public class AccountLoginQueries
+    {
+        public string ConnectionString { get; set; }
+
+        public AccountLoginQueries()
+        {
+            this.ConnectionString = "Server=studmysql01.fhict.local;Uid=dbi437981;Database=dbi437981;Pwd=Dierenasiel;";
+        }
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
