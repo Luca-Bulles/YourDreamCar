adc35396ebc00a80d41bcbbc2da443a61a22736c
Luca Bulles <lucabulles@gmail.com>
2020-11-13 16:13:28 +0100

    Added Interface for AccountLoginQueries

--- YourDreamCarDAL/Queries/AccountLoginQueries.cs
+++ YourDreamCarDAL/Queries/AccountLoginQueries.cs
@@ -2,10 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using YourDreamCarInterfaces.DAL;
 
 namespace YourDreamCarDAL.Queries
 {
-    public class AccountLoginQueries
+    public class AccountLoginQueries : IAccountLoginQueries
     {
         public string ConnectionString { get; set; }
 
@@ -17,6 +18,35 @@ namespace YourDreamCarDAL.Queries
         {
             return new MySqlConnection(ConnectionString);
         }
+        public bool Login(ILogin user)
+        {
+            bool login = false;
 
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                using (MySqlCommand query = new MySqlCommand("SELECT * FROM cars_account WHERE Email = @Email AND Password = SHA1(@Password);", conn))
+                {
+                    query.Parameters.AddWithValue("@Email", user.Email);
+                    query.Parameters.AddWithValue("@Password", user.Password);
+
+                    var reader = query.ExecuteReader();
+                    //controle of de user bestaat
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            user.UserId = reader.GetInt32(0);
+                            user.Email = reader.GetString(1);
+                        }
+                        login = true;
+                    }
+
+                }
+                conn.Close();
+            }
+            return login;
+            
+        }
     }
 }
---
+++ YourDreamCarInterfaces/DAL/IAccountLoginQueries.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YourDreamCarInterfaces.DAL
+{
+    public interface IAccountLoginQueries
+    {
+        bool Login(ILogin user);
+    }
+}
