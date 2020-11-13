d9b322792e8c1d84394bd61c657a76c17a46a1d5
Luca Bulles <lucabulles@gmail.com>
2020-11-13 16:20:27 +0100

    Added Interface for LoginLogic

---
+++ YourDreamCarLogic/Account/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using YourDreamCarInterfaces.DAL;
+
+namespace YourDreamCarLogic.Account
+{
+    public class Login : ILogin
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Input a valid Emailaddress")]
+        [Display(Name = "Emailaddress")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Input a valid password")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
